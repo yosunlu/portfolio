cb552b4884af974afdeb96c13aebaecdb05a4fef
yushanlu <yushanlu1031@gmail.com>
2024-03-30 13:27:05 -0500

    motion for header

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
+import { links } from "@/lib/data"
+import Link from 'next/link';
 
 export default function Header() {
   return (
@@ -12,9 +14,23 @@ export default function Header() {
             initial={{ y: -100, x: "-50%", opacity: 0}}
             animate={{ y: 0, x: "-50%", opacity: 1}}>
         </motion.div>
+        
+        <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+            <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+                {links.map((link) => (
+                    <motion.li className="h-3/4 flex items-center justify-center" key={link.hash} initial={{ y: -100, opacity: 0}} animate={{ y: 0, opacity: 1}}>
+                            <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300" href={link.hash}>
+                                {link.name}
+                            </Link>
+                    </motion.li>
+               ))}
+            </ul>
+        </nav>
     </header>
   );
   
 }
 
 // y: -100, x: "-50%", opacity: 0  : -50% makes the header middle
+// <li className="h-3/4" key={link.hash}> : this makes its height 3/4 of parent element
+// left-1/2 把component左邊對其正中間; -transalte-x-1/2 把這個這個component正中心往左移他的w的1/2
