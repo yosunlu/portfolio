c550a2c657983b4288c5c1457e15a577fa352e6e
yushanlu <yushanlu1031@gmail.com>
2024-04-16 01:57:50 -0500

    Skills Done #7

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from '@/components/about'
 import Intro from '@/components/intro'
 import SectionDevider from '@/components/section-devider'
 import Projects from '@/components/projects'
+import Skills from '@/components/skills'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDevider/>
       <About/>
       <Projects/>
+      <Skills/>
     </main>
   )
 }
