0e2b213e059b785655ca8e174b8c63f0b4bed981
yushanlu <yushanlu1031@gmail.com>
2024-04-18 03:22:54 -0500

    footer done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import SmoothScrolling from "@/components/smoothScrolling";
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           <Header/>        
           {children}
           <Toaster position='top-right'/>
+          <Footer/>
         </ActiveSectionContextProvider>
         
       </body>
